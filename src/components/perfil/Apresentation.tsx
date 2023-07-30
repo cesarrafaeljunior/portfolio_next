8761494dc0db9bd49932d0510e836e7c6671584a
César Rafael <cesarrdev@gmail.com>
2023-07-30 11:11:02 -0300

    feat: Add icon tag in apresentation component

--- src/components/perfil/Apresentation.tsx
+++ src/components/perfil/Apresentation.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image"
+import {FaLessThan} from "react-icons/fa"
+import {FaGreaterThan} from "react-icons/fa"
 import pefilIcon from "../../assets/img/perfil_img.jpg"
 
 export const Apresentation = () => {
@@ -10,10 +12,18 @@ export const Apresentation = () => {
                     <Image src={pefilIcon} alt="perfil image " width={500} height={500} quality={70} className="w-full h-full rounded-full object-fit"/>
                 </figure>
             </div>
-            <div className="flex flex-col gap-[1rem]">
-                <h1><span className="text-4xl text-violet-700 phone:text-[2rem]">Olá</span>,<span className="text-[2.2rem] phone:text-[1.5rem]"> eu sou o César</span></h1>
-                <p className="text-violet-600 phone:text-center">Desenvolvedor Full Stack</p>
-                <p className="text-gray-400 phone:text-center">&quot;Conectando tecnologia e inovação, uma linha de código por vez&quot;</p>
+            <div>
+                <div className="flex gap-[0.2rem] text-violet-700">
+                    <FaLessThan/><FaGreaterThan/>  
+                </div>
+                <div className="flex flex-col gap-[1rem] ml-[1rem] my-[1rem] phone:items-start">
+                    <h1><span className="text-4xl text-violet-700 phone:text-[2rem]">Olá</span>,<span className="text-[2.2rem] phone:text-[1.5rem]"> eu sou o César</span></h1>
+                    <p className="text-violet-600">Desenvolvedor Full Stack</p>
+                    <p className="text-gray-400">&quot;Conectando tecnologia e inovação, uma linha de código por vez&quot;</p>        
+                </div>         
+                <div className="flex gap-[0.2rem] text-violet-700 items-center">
+                    <FaLessThan/><span className="text-[1.5rem] font-extrabold">/</span><FaGreaterThan/>  
+                </div>
             </div>
             </div>
         </div>
--- src/components/perfil/Formation.tsx
+++ src/components/perfil/Formation.tsx
@@ -14,11 +14,11 @@ export const FormationInfos = () => {
                 <p className="w-[50%] text-gray-400 phone:w-[100%]  phone:text-center">Formado pela instituição Kenzie Academy Brasil como <strong>Técnico em programação</strong></p>
                 <h3 className="text-[1.2rem] my-[2.5rem] font-bold">Principais tecnologias</h3>
                 <div className="w-[50%] flex gap-[2rem] phone:w-[100%] phone:overflow-x-auto phone:px-[1rem] phone:py-[1rem]">
-                    <IconTech icon={BiLogoJavascript} percentage="100%" title="JavaScript"/>
-                    <IconTech icon={BiLogoReact} percentage="100%" title="React"/>
-                    <IconTech icon={TbDatabaseSearch} percentage="100%" title="Sql"/>
-                    <IconTech icon={BiLogoPython} percentage="100%" title="Python"/>
-                    <IconTech icon={BiLogoNodejs} percentage="100%" title="Node.js"/>
+                    <IconTech icon={BiLogoJavascript} title="JavaScript"/>
+                    <IconTech icon={BiLogoReact} title="React"/>
+                    <IconTech icon={TbDatabaseSearch} title="Sql"/>
+                    <IconTech icon={BiLogoPython} title="Python"/>
+                    <IconTech icon={BiLogoNodejs} title="Node.js"/>
                 </div>
                 <button className="w-[240px] ml-[5rem] mt-[1rem] p-[0.5rem] rounded-[20px] font-bold text-slate-50 transition ease-in-out delay-[0.1s] hover:text-violet-300 phone:ml-[0]">Ver todas as competências</button>
             </div>
